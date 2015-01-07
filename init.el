d047b804c94a689e0bd739da2fedf80acb1878cb
Peter Schmiedeskamp <peter@thoughtspot.net>
2015-01-07 15:00:41 -0800

    pull package config into org settings

--- init.el
+++ init.el
@@ -4,21 +4,6 @@
 ;; Allow emacs to start a server so that EDITOR environment can be set
 (server-start)
 
-;; Set up the melpa package repository
-(require 'package)
-(setq package-user-dir "~/.emacs.d/elpa/")
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-;; Install packages required for bootstrap (probably just org)
-(defvar bootstrap-packages '(org) "Packages required for bootstrap")
-(dolist (p bootstrap-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
-
 ;; Bootstrap the remainder of our configuration
 (require 'org)
 (org-babel-load-file
