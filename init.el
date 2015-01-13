a5378ec038c8aa88b4736407ca5e5bb7a7ff609d
Peter Schmiedeskamp <peter@thoughtspot.net>
2015-01-13 12:55:05 -0800

    misc init changes; org-babel config; mu4e cc mail address matching

--- init.el
+++ init.el
@@ -4,8 +4,13 @@
 ;; Allow emacs to start a server so that EDITOR environment can be set
 (server-start)
 
+;; Initialize packages (so org can work)
+(setq package-enable-at-startup nil)
+(package-initialize)
+
 ;; Bootstrap the remainder of our configuration
 (require 'org)
 (org-babel-load-file
  (expand-file-name "settings.org"
                    user-emacs-directory))
+
